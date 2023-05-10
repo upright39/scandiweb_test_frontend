a91bfd5442960211f1f30c5ee7ce2104282025ba
upright <uprightnzubechi@gmail.com>
2023-05-10 05:06:33 -0700

    next commit

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,9 +5,9 @@ function Products({ sku, names, price, type, details, checked, onChange }) {
     <>
         <div className="card h190 m15 p-10">
             <input type="checkbox" checked={checked} onChange={onChange}  className="delete-checkbox top20 left20"/>   
-            <h4>{sku}</h4>
+            <p className="uppercase">{sku}</p>
             <p>{names}</p>
-            <p>$ {price}</p>
+            <p>{price} $</p>
             <p>{details}</p>
         </div>
     </>
--- src/validation/formValidation.js
+++ src/validation/formValidation.js
@@ -4,35 +4,50 @@ const formValidation = (types, sku, names, price, size,length,weight,width,heigh
     if (!sku) {
       errors.sku = "SKU is required";
     }
-  
+
     if (!names) {
       errors.names = "Name is required";
+    } else if (!/^[a-zA-Z\s]+$/.test(names)) {
+      errors.names = "Name must only contain letters and spaces";
     }
-  
+
+
+
     if (!price) {
-      errors.price = "Price is required";
+      errors.price = "Price is required.";
+    } else if (isNaN(price)) {
+      errors.price = "Price must be a valid number.";
     }
+
     if (!types) {
         errors.type = "product type is required";
       }
 
-
      if (types === "DVD") {
       if(!size){
         errors.size = "Size is required";
-      }    
+      } else if(isNaN(size)){
+        errors.size = "height must be a valid number"
+      }   
     }
   
     if (types === "Furniture") {
       if (!height) {
         errors.height = "height is required";
+      }else if (isNaN(height)) {
+        errors.height = "height must be a valid number.";
       }
   
       if (!width) {
         errors.width = "width is required";
+      }else if (isNaN(width)) {
+        errors.width = "width must be a valid number.";
       }
+
       if (!length) {
         errors.length = "length is required";
+      }else if (isNaN(length)) {
+        errors.length = "length must be a valid number.";
       }
     }
 
@@ -40,8 +55,9 @@ const formValidation = (types, sku, names, price, size,length,weight,width,heigh
 
         if(!weight){
             errors.weight = "weight is required";
-        }
-       
+        }else if(isNaN(weight)){
+          errors.weight = "weight must be a valid number";
+        }    
       }
   
     return errors;
