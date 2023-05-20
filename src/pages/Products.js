eb8ea6d2856b485a5f3bf36350758e9f0f908bec
upright <uprightnzubechi@gmail.com>
2023-05-20 11:50:46 -0700

    next commit

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,7 +3,7 @@ function Products({ sku, name, price, details, checked, onChange }) {
     return (
         <>
             <div className="card h190 m15 p-10">
-                <input className="delete-checkbox top20 left20" type="checkbox" checked={checked} onChange={onChange}  />
+                <input className="top20 left20 delete-checkbox" type="checkbox" checked={checked} onChange={onChange}  />
                 <p className="uppercase">{sku}</p>
                 <p>{name}</p>
                 <p>{price} $</p>
