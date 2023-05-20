2439a705a197eba6ac4979ddddbf6cce256f2b17
upright <uprightnzubechi@gmail.com>
2023-05-20 12:44:27 -0700

    next next

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -15,7 +15,7 @@ const ProductPage = () => {
 
   const fetchItems = async () => {
     try {
-      const response = await axios.get('http://localhost/test-scandiweb/api/product/read_product.php');
+      const response = await axios.get('https://upright-scandiweb.000webhostapp.com/api/product/read_product.php');
       setItems(response.data.data);     
       if (response.data.message === "No Post Found") {
         setItems([]);
